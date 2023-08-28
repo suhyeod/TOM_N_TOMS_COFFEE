fbc638231cd1373c7ae407e8a61ecfe5ded9a14f
suhyeod <133014897+suhyeod@users.noreply.github.com>
2023-08-28 17:46:13 +0900

    Add files via upload

---
+++ 탐앤탐스 html/js/js.js
@@ -0,0 +1,17 @@
+const silder = document.querySelectorAll("silder_1") //전체 슬라이드
+const silder_1 = document.querySelectorAll("silder_1 > li")
+
+
+
+
+
+function moveSilde(event){
+    window.setInterval(()=>{
+        silder_1
+    },1000)
+}
+
+
+// window.setInterval(()=>{
+//     silder_1.moveSilder
+//     },3000)
