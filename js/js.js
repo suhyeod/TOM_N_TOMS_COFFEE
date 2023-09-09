b6aaa38c3821211922afea70b8261da4d6ac6be8
suhyeod <133014897+suhyeod@users.noreply.github.com>
2023-09-09 18:17:49 +0900

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,94 @@
+// op
+const dataElements = document.querySelectorAll('.op');
+window.addEventListener('scroll', function () {
+    dataElements.forEach(function (element) {
+        if (isElementInViewport(element)) {
+            element
+                .classList
+                .add('show'); 
+        }
+    });
+});
+
+
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+function openhelpitem(e, h) {
+    if (e.lastElementChild.style.height == '' || e.lastElementChild.style.height == '0px') {
+        console.log('aaa')
+        e.lastElementChild.style.height = h + "px"
+    } else {
+        console.log('bbb')
+        e.lastElementChild.style.height = '0px';
+    }
+}
+//op 끝
+
+
+//무한 슬라이드
+
+$(document).ready(function(){
+	var height =  $(".notice").height(); 
+	var num = $(".rolling_1 > li").length; 
+	var max = height * num; 
+	var move = 0; 
+	function noticeRolling(){
+		move += height; 
+		$(".rolling_1").animate({"top":-move},600,function(){ 
+			if( move >= 6 ){ 
+				$(this).css("top",0); 
+				move = 0; 
+			};
+		});
+	};
+	noticeRollingOff = setInterval(noticeRolling,3000);
+	$(".rolling_1").append($(".rolling_1 > li").first().clone()); 
+
+	$(".rolling_stop").click(function(){
+		clearInterval(noticeRollingOff); 
+	});
+	$(".rolling_start").click(function(){
+		noticeRollingOff = setInterval(noticeRolling,1000); 
+	});
+});
+
+$(document).ready(function(){
+	var height =  $(".notice").height(); 
+	var num = $(".rolling_2 > li").length; 
+	var max = height * num; 
+	var move = 0; 
+	function noticeRolling(){
+		move += height; 
+		$(".rolling_2").animate({"top":-move},600,function(){ 
+			if( move >= 6 ){ 
+				$(this).css("top",0); 
+				move = 0; 
+			};
+		});
+	};
+	noticeRollingOff = setInterval(noticeRolling,3000);
+	$(".rolling_2").append($(".rolling_2 > li").first().clone()); 
+
+	$(".rolling_stop").click(function(){
+		clearInterval(noticeRollingOff); 
+	});
+	$(".rolling_start").click(function(){
+		noticeRollingOff = setInterval(noticeRolling,1000); 
+	});
+});
+
+
+
+//무한 슬라이드
+
+
+
+$(function(){
+	$(".onc > li > a").hover(function (){
+		$(this).siblings().stop().slideToggle();
+	});
+});
